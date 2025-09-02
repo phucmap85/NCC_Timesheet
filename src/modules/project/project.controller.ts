e87f86a21d4221074664e31ed2193a646e3cbdcb
Le Quang Phuc <phuclequang2006@gmail.com>
2025-09-02 09:55:17 +0700

    Add endpoints for updating and clearing default project task

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, Request, HttpCode, Query, ParseIntPipe, DefaultValuePipe, Delete } from '@nestjs/common';
 import type { Request as ExpressRequest } from 'express';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { ProjectDto } from 'src/modules/project/project.dto';
 import { ProjectService } from 'src/modules/project/project.service';
 
@@ -51,6 +52,20 @@ export class ProjectController {
   @HttpCode(200)
   async inactiveProject(@Body('id', ParseIntPipe) id: number): Promise<object | null> {
     return await this.projectService.inactiveProject(id);
+  }
+
+  @Post("UpdateDefaultProjectTask")
+  @HttpCode(200)
+  @ApiOkResponse({ type: Boolean })
+  async updateDefaultProjectTask(): Promise<boolean> {
+    return true;
+  }
+
+  @Post("ClearDefaultProjectTask")
+  @HttpCode(200)
+  @ApiOkResponse({ type: Boolean })
+  async clearDefaultProjectTask(): Promise<boolean> {
+    return true;
   }
 
   @Delete("Delete")
